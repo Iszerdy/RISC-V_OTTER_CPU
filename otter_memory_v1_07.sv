00c2efe6191ffc683d3b2a02d61e3a6d7d47d814
Isabel Szerdy <71780677+Iszerdy@users.noreply.github.com>
2026-01-18 10:37:35 -0800

    Update otter_memory_v1_07.sv

--- otter_memory_v1_07.sv
+++ otter_memory_v1_07.sv
@@ -1,51 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer: J. Callenes, P. Hummel
-//
-// Create Date: 01/27/2019 08:37:11 AM
-// Module Name: OTTER_mem
-// Project Name: Memory for OTTER RV32I RISC-V
-// Tool Versions: Xilinx Vivado 2019.2
-// Description: 64k Memory, dual access read single access write. Designed to
-//              purposely utilize BRAM which requires synchronous reads and write
-//              ADDR1 used for Program Memory Instruction. Word addressable so it
-//              must be adapted from byte addresses in connection from PC
-//              ADDR2 used for data access, both internal and external memory
-//              mapped IO. ADDR2 is byte addressable.
-//              RDEN1 and EDEN2 are read enables for ADDR1 and ADDR2. These are
-//              needed due to synchronous reading
-//              MEM_SIZE used to specify reads as byte (0), half (1), or word (2)
-//              MEM_SIGN used to specify unsigned (1) vs signed (0) extension
-//              IO_IN is data from external IO and synchronously buffered
-//
-// Memory OTTER_MEMORY (
-//    .MEM_CLK   (),
-//    .MEM_RDEN1 (),
-//    .MEM_RDEN2 (),
-//    .MEM_WE2   (),
-//    .MEM_ADDR1 (),
-//    .MEM_ADDR2 (),
-//    .MEM_DIN2  (),
-//    .MEM_SIZE  (),
-//    .MEM_SIGN  (),
-//    .IO_IN     (),
-//    .IO_WR     (),
-//    .MEM_DOUT1 (),
-//    .MEM_DOUT2 ()  );
-//
-// Revision:
-// Revision 0.01 - Original by J. Callenes
-// Revision 1.02 - Rewrite to simplify logic by P. Hummel
-// Revision 1.03 - changed signal names, added instantiation template
-// Revision 1.04 - added defualt to write case statement
-// Revision 1.05 - changed MEM_WD to MEM_DIN2, changed default to save nothing
-// Revision 1.06 - removed typo in instantiation template
-// Revision 1.07 - remove unused wordAddr1 signal
-//
-//////////////////////////////////////////////////////////////////////////////////
-         
- 
 
 module Memory (
     input logic MEM_CLK,
@@ -75,4 +28,5 @@ module Memory (
     end
 
 endmodule
+
  
